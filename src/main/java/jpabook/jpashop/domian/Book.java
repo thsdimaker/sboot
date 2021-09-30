b799256a2530d04fbccd181af6e82ae81cc09fce
DG <thsdimaker@gmail.com>
2021-09-30 23:01:16 +0900

    update

--- src/main/java/jpabook/jpashop/domian/Book.java
+++ src/main/java/jpabook/jpashop/domian/Book.java
@@ -1,8 +1,15 @@
 package jpabook.jpashop.domian;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 
+@Entity
+@DiscriminatorValue("B")
+@Getter@Setter
 public class Book extends Item{
 
     private String author;
