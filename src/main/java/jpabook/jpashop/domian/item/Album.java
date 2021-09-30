d4fc5ed0e71728454519b7246efbf1ff61fa1dff
DG <thsdimaker@gmail.com>
2021-09-30 23:04:00 +0900

    update

--- src/main/java/jpabook/jpashop/domian/item/Album.java
+++ src/main/java/jpabook/jpashop/domian/item/Album.java
@@ -1,7 +1,15 @@
 package jpabook.jpashop.domian.item;
 
 import jpabook.jpashop.domian.Item;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("A")
+@Getter @Setter
 public class Album extends Item {
 
     private String artist;
