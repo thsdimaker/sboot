aafe0f7dc933566a50043f170e477ae328ebf2ba
DG <thsdimaker@gmail.com>
2021-10-01 21:25:25 +0900

    update

--- src/main/java/jpabook/jpashop/domian/Category.java
+++ src/main/java/jpabook/jpashop/domian/Category.java
@@ -1,4 +1,34 @@
 package jpabook.jpashop.domian;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
 public class Category {
+
+    private Long id;
+    private String name;
+
+
+
+    private List<Item> items = new ArrayList<>();
+
+    private Category parent;
+
+    private List<Category> child = new ArrayList<>();
+
+    /*
+    RelationShips Method
+    */
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
+
 }
