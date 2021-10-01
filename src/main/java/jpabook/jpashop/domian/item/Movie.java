30c52a6e517375133d5462fe3d8b13dc8434ba56
DG <thsdimaker@gmail.com>
2021-10-01 21:21:03 +0900

    update

--- src/main/java/jpabook/jpashop/domian/item/Movie.java
+++ src/main/java/jpabook/jpashop/domian/item/Movie.java
@@ -1,7 +1,15 @@
 package jpabook.jpashop.domian.item;
 
 import jpabook.jpashop.domian.Item;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("M")
+@Getter @Setter
 public class Movie extends Item {
 
     private String director;
