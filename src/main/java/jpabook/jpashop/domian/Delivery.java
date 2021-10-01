ad749602ae283d57806ee3479a73189fce471969
DG <thsdimaker@gmail.com>
2021-10-01 21:22:44 +0900

    update

--- src/main/java/jpabook/jpashop/domian/Delivery.java
+++ src/main/java/jpabook/jpashop/domian/Delivery.java
@@ -3,13 +3,23 @@ package jpabook.jpashop.domian;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
 @Getter @Setter
 public class Delivery {
 
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
     private Long id;
 
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
+    @Embedded
     private Address address;
+
+    @Enumerated
+    private DeliveryStatus status;
 
 }
