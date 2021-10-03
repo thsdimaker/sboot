f50f19e9a5117e61406fbd493b6fcf98adb7ce07
DG <thsdimaker@gmail.com>
2021-10-03 09:01:08 +0900

    update

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -1,5 +1,38 @@
 package jpabook.jpashop.service;
 
 
+import jpabook.jpashop.domian.Member;
+import jpabook.jpashop.repository.MemberRepository;
+
+import java.util.List;
+
 public class MemberService {
+
+    MemberRepository memberRepository;
+
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
 }
