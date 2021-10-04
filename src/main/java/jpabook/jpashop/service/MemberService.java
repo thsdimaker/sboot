c382f22064196bb185c6f50a584db208e80270fc
DG <thsdimaker@gmail.com>
2021-10-04 10:34:02 +0900

    update

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domian.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +11,17 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
 
-    MemberRepository memberRepository;
+//    @Autowired
+//    MemberRepository memberRepository;
+
+    private final MemberRepository memberRepository;
+
+//    public MemberService(MemberRepository memberRepository){
+//        this.memberRepository = memberRepository;
+//    }
 
     public Long join(Member member){
         validateDuplicateMember(member);
