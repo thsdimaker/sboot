74d2d097de748710763db6fb0ea1385bd81e588f
DG <thsdimaker@gmail.com>
2021-10-05 21:03:12 +0900

    create

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MemberServiceTest {
+
+    @Test
+    public void 회원가입() throws Exception {
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+    }
+
+
+}
