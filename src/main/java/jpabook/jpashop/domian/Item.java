a0eead035ed7abffb222a1cf355c3d44a04c4c5e
DG <thsdimaker@gmail.com>
2021-10-07 20:48:56 +0900

    update

--- src/main/java/jpabook/jpashop/domian/Item.java
+++ src/main/java/jpabook/jpashop/domian/Item.java
@@ -18,10 +18,22 @@ public abstract class Item {
     private Long id;
 
     private String name;
-
     private int price;
-
     private int stockQuantity;
 
+    @ManyToMany(mappedBy = "items")
     private List<Category> categorie = new ArrayList<Category>();
+
+    /**
+    * Business Logic
+    */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
 }
