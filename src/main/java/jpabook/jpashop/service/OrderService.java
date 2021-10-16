c778632d23f19bd84c6ac1ca0e85fa38710f393f
DG <thsdimaker@gmail.com>
2021-10-16 23:14:23 +0900

    update

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -4,13 +4,20 @@ import jpabook.jpashop.domian.*;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class OrderService {
 
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
     private final ItemRepository itemRepository;
 
+    @Transactional
     public Long order(Long memberId, Long itemId, int count) {
 
         // 엔티티 조회
@@ -35,6 +42,7 @@ public class OrderService {
     }
 
     /** 주문 취소 */
+    @Transactional
     public void cancelOrder(Long orderId) {
         //주문 엔티티 조회
         Order order = orderRepository.findOne(orderId);
@@ -47,8 +55,4 @@ public class OrderService {
  return orderRepository.findAll(orderSearch);
  }
 */
-
-
-
-
 }
