47245ce60b916300d6f586617386a888f58a0469
DG <thsdimaker@gmail.com>
2021-10-17 20:07:49 +0900

    create

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderServiceTest {
+
+    @Test
+    public void 상품주문() throws Exception {
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+    }
+}
