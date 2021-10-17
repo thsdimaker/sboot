c0d5f4a72eb2d0cc755ad13179f921f5db2a3954
DG <thsdimaker@gmail.com>
2021-10-17 20:12:06 +0900

    update

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -51,7 +51,17 @@ public class OrderServiceTest {
 
     @Test(expected = NotEnoughStockException.class)
     public void 상품주문_재고수량초과() throws Exception {
-        //...
+        //Given
+        Member member = createMember();
+        Item item = createBook("시골 JPA", 10000, 10); //이름, 가격, 재고
+        int orderCount = 11; //재고보다 많은 수량
+
+        //When
+        orderService.order(member.getId(), item.getId(), orderCount);
+
+        //Then
+        fail("재고 수량 부족 예외가 발생해야 한다.");
+
     }
 
     @Test
