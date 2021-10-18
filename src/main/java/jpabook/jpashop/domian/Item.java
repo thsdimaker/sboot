4313fc41d708972d7797aa9a8b105733220c2e2d
DG <thsdimaker@gmail.com>
2021-10-18 22:46:38 +0900

    update

--- src/main/java/jpabook/jpashop/domian/Item.java
+++ src/main/java/jpabook/jpashop/domian/Item.java
@@ -40,4 +40,6 @@ public abstract class Item {
         }
         this.stockQuantity = restStock;
     }
+
+
 }
--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -65,8 +65,23 @@ public class OrderServiceTest {
     }
 
     @Test
-    public void 주문취소() throws Exception{
+    public void 주문취소() throws Exception {
 
+        //Given
+        Member member = createMember();
+        Item item = createBook("시골 JPA", 10000, 10); //이름, 가격, 재고
+        int orderCount = 2;
+
+        Long orderId = orderService.order(member.getId(), item.getId(),
+                orderCount);
+        //When
+        orderService.cancelOrder(orderId);
+        //Then
+        Order getOrder = orderRepository.findOne(orderId);
+        assertEquals("주문 취소시 상태는 CANCEL 이다.",OrderStatus.CANCEL,
+                getOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10,
+                item.getStockQuantity());
     }
 
     private Member createMember() {
