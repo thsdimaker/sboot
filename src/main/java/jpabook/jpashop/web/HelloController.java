94f4e9b0ba613efbf52691d98f65d4dfb75a6d05
DG <thsdimaker@gmail.com>
2021-10-25 22:02:54 +0900

    update

--- src/main/java/jpabook/jpashop/web/HelloController.java
+++ src/main/java/jpabook/jpashop/web/HelloController.java
@@ -1,15 +1,18 @@
 package jpabook.jpashop.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class HelloController {
 
-    @GetMapping("hello")
-    public String hello(Model model){
-        model.addAttribute("data", "hello!");
+    @RequestMapping("/")
+    public String home(){
+        log.info(" home controller");
         return "hello";
     }
 }
